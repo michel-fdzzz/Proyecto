a9debf45aed53871699409a26a7fc162735e29dc
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-04-05 17:02:09 +0200

    Revisar contraseña para que la revise

--- PROYECTO/conexion.php
+++ PROYECTO/conexion.php
@@ -1,18 +1,19 @@
 <?php
-    // Esta clase será incluida en todos los archivos para conectarnos a la base de datos
-    class Conexion {
-        private $host = "localhost";
-        private $usuario = "root";
-        private $contraseña = "nikita01C#";
-        private $base_datos = "tienda";
+// Esta clase será incluida en todos los archivos para conectarnos a la base de datos
+class Conexion
+{
+    private $host = "localhost";
+    private $usuario = "root";
+    private $contraseña = "";
+    private $base_datos = "tiendaRelojes";
 
-        // Hace la conexión con la base de datos
-        public function conectar() {
-            $con = new mysqli($this->host, $this->usuario, $this->contraseña, $this->base_datos);
-            if ($con->connect_error) {
-                die("Error de conexión: " . $con->connect_error);
-            }
-            return $con;
+    // Hace la conexión con la base de datos
+    public function conectar()
+    {
+        $con = new mysqli($this->host, $this->usuario, $this->contraseña, $this->base_datos);
+        if ($con->connect_error) {
+            die("Error de conexión: " . $con->connect_error);
         }
+        return $con;
     }
-?>
+}
