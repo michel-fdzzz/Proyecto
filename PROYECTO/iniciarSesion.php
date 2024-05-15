32dde120ab978858ccbded6b9c7b6adb8e46d5d6
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-05-15 17:01:58 +0200

    nueva altura de cabecera y responsive los formularios

--- PROYECTO/iniciarSesion.php
+++ PROYECTO/iniciarSesion.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrarse</title>
-    <link href="CSS/registro.css" rel="stylesheet" type="text/css">
+    <link href="CSS/iniciarSesion.css" rel="stylesheet" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 </head>
