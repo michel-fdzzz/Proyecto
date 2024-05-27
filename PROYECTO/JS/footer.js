b16beeeae1a2b65de413b174477d0abfe2e553cb
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-05-27 22:16:48 +0200

    Se muestra lo del footer con el jQuery

--- PROYECTO/JS/footer.js
+++ PROYECTO/JS/footer.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+/*document.addEventListener('DOMContentLoaded', function () {
     let leyes = document.querySelector('.leyes');
     leyes.addEventListener('click', function () {
         document.querySelector('.leyes-contenedor').style.display = 'block';
@@ -23,5 +23,37 @@ document.addEventListener('DOMContentLoaded', function () {
     cerrar_info_contacto.addEventListener('click', function () {
         document.querySelector('.info-contacto-contenedor').style.display = 'none';
         document.querySelector('.info-contacto-contenedor').classList.remove('mostrar');
+    });
+});*/
+$(document).ready(function () {
+    // Mostrar el contenedor de la ley al hacer clic en el elemento con clase "leyes"
+    $(".leyes").click(function () {
+        $(".leyes-contenedor").fadeIn();
+    });
+
+    // Cerrar el contenedor de la ley al hacer clic en el botón de cierre
+    $(".cerrar").click(function () {
+        $(".leyes-contenedor").fadeOut();
+    });
+
+    // Mostrar el contenedor de información de contacto al hacer clic en el elemento con clase "info-contacto"
+    $(".info-contacto").click(function () {
+        $(".info-contacto-contenedor").fadeIn();
+    });
+
+    // Cerrar el contenedor de información de contacto al hacer clic en el botón de cierre
+    $(".cerrar-info-contacto").click(function () {
+        $(".info-contacto-contenedor").fadeOut();
+    });
+
+    // Mostrar el contenedor "Quiénes somos" al hacer clic en el elemento con clase "quienes-somos"
+    $(".quienes-somos").click(function () {
+        alert('ds')
+        $(".quienes-somos-contenedor").fadeIn();
+    });
+
+    // Cerrar el contenedor "Quiénes somos" al hacer clic en el botón de cierre
+    $(".cerrar-quienes-somos").click(function () {
+        $(".quienes-somos-contenedor").fadeOut();
     });
 });
--- PROYECTO/footer.php
+++ PROYECTO/footer.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="CSS/footer.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script defer src='JS/footer.js'></script>
 </head>
 <body>
@@ -28,8 +29,8 @@
                 </div>
 
                 <div class="contenedor-privacidad">
-                    <h1>Privacidad</h1>
-                    <p>Leyes</p>
+                    <h1>Sobre Nosotros</h1>
+                    <p class='quienes-somos'>Quienes somos</p>
                     <p>Licencias</p>
                 </div>
             </article>
@@ -41,8 +42,8 @@
 
 
            <!-- Agregamos un contenedor para la ventana emergente -->
-    <div class="leyes-contenedor" style="display: none;">
-        <div class="contenedor-cont-apratados-footer">
+    <div class="leyes-contenedor">
+        <div class="contenedor-cont-apartados-footer">
             <!-- Agregamos el botón de cierre -->
             <span class="cerrar">&times;</span>
             <!-- Aquí se mostrará la información de la ley -->
@@ -50,9 +51,9 @@
             <p class='sin-margen'>La Ley de Protección de Datos Digitales de 2024 tiene como objetivo salvaguardar la privacidad y los derechos de los ciudadanos en el ámbito digital. Establece principios fundamentales, como la legalidad y transparencia en el manejo de datos personales, el consentimiento explícito para su recopilación y tratamiento, así como el derecho de los individuos a acceder, corregir y eliminar su información. Además, impone responsabilidades a las entidades que manejan datos y establece sanciones para aquellos que infrinjan la ley. Una autoridad independiente supervisará su cumplimiento y los ciudadanos tendrán derecho a presentar quejas en caso de violación de sus derechos de privacidad.</p>
         </div>
     </div>
-
-    <div class="info-contacto-contenedor" style="display: none;">
-        <div class="contenedor-cont-apratados-footer">
+    
+    <div class="info-contacto-contenedor">
+        <div class="contenedor-cont-apartados-footer">
             <!-- Agregamos el botón de cierre -->
             <span class="cerrar-info-contacto">&times;</span>
             <!-- Aquí se mostrará la información de la ley -->
@@ -61,6 +62,30 @@
                 <div class="email">Correo: <a href="mailto:Michel&CO@gmail.com">Michel&CO@gmail.com</a></div>
                 <div class="telefono">Atención al cliente: <a href="tel:+34678574678">678574678</a></div>
             <div>
+        </div>
+    </div>
+</div>
+    
+    <div class="quienes-somos-contenedor">
+        <div class="contenedor-cont-apartados-footer">
+            <!-- Agregamos el botón de cierre -->
+            <span class="cerrar-quienes-somos">&times;</span>
+            <!-- Aquí se mostrará la información de la ley -->
+            <h1>Quienes somos</h1>
+            <p class='sin-margen'>
+
+            En el corazón de Madrid se alza la boutique más exclusiva de relojes: "Michel & Co". Con sus paredes de cristal reluciente y una decoración que irradia elegancia y lujo, esta tienda es el destino definitivo para los amantes de los relojes de alta gama.
+
+            La historia de "Michel & Co" se remonta a décadas atrás, cuando el joven emprendedor, Michel, fascinado por la artesanía y la precisión de los relojes suizos, decidió convertir su pasión en su profesión. Comenzó su negocio en un modesto local, donde vendía relojes de marcas reconocidas, pero pronto se ganó una reputación por su atención al detalle y su dedicación a proporcionar a sus clientes las piezas más exquisitas.
+
+            Con el tiempo, la tienda se convirtió en un referente en el mundo de la relojería de alta gama. Michel viajaba por todo el mundo en busca de las últimas creaciones de las mejores marcas de relojes, desde los clásicos atemporales hasta las innovadoras obras maestras de la relojería contemporánea. Su habilidad para seleccionar cuidadosamente cada pieza, asegurándose de que reflejara la calidad y el estilo que definían a "Michel & Co", le valió el respeto y la lealtad de una clientela fiel y distinguida.
+
+            Pero "Michel & Co" no era solo una tienda de relojes; era un lugar donde se contaban historias. Cada reloj tenía su propia narrativa: un regalo de aniversario, un símbolo de éxito profesional, una herencia familiar. Michel y su equipo no solo vendían relojes, sino que también ayudaban a sus clientes a encontrar la pieza perfecta que encajara con su estilo de vida y personalidad.
+
+            La boutique se convirtió en un punto de encuentro para coleccionistas, aficionados y curiosos por igual. Eventos exclusivos, lanzamientos de colecciones y charlas sobre la historia de la relojería se celebraban regularmente en el elegante espacio de "Michel & Co", donde la pasión por los relojes se compartía y celebraba entre amigos.
+
+            Con el paso de los años, "Michel & Co" se ha convertido en mucho más que una tienda de relojes; es un destino de referencia para aquellos que buscan algo más que una simple medición del tiempo. Es un lugar donde la artesanía se encuentra con la historia, donde el lujo se combina con la pasión y donde cada reloj cuenta una historia única que perdura en el tiempo.
+            </p>
         </div>
     </div>
     </footer>
