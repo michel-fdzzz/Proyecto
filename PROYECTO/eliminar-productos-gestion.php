96f9907fdaae770fe5027916453f1ca7c6e1ced4
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-06-11 14:38:20 +0200

    Ordenar por orden lafabetico por marca

--- PROYECTO/eliminar-productos-gestion.php
+++ PROYECTO/eliminar-productos-gestion.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,36 +8,37 @@
     <link rel="stylesheet" href="CSS/eliminar-producto-gestion.css">
     <script defer src='JS/eliminar-gestion.js'></script>
 </head>
+
 <body>
     <?php
     include 'header.php';
     ?>
 
     <section class="main">
-    <article class="container-productos">
-            
-    <?php
-    $con = new Conexion();
-    $con = $con->conectar();
-    
-    $select = "SELECT id, imagen, nombre, marca
+        <article class="container-productos">
+
+            <?php
+            $con = new Conexion();
+            $con = $con->conectar();
+
+            $select = "SELECT id, imagen, nombre, marca
             FROM producto
-            ORDER BY nombre DESC";
-    $stmt = $con->prepare($select);
+            ORDER BY marca DESC";
+            $stmt = $con->prepare($select);
 
-    if ($stmt) {
-        $stmt->execute();
-        $result = $stmt->get_result();
+            if ($stmt) {
+                $stmt->execute();
+                $result = $stmt->get_result();
 
-        if ($result->num_rows > 0) {
-            while ($fila = $result->fetch_assoc()) {
-                
-                $id = $fila['id'];
-                $imagen = $fila['imagen'];
-                $nombre = $fila['nombre'];
-                $marca = $fila['marca'];
+                if ($result->num_rows > 0) {
+                    while ($fila = $result->fetch_assoc()) {
 
-                echo "
+                        $id = $fila['id'];
+                        $imagen = $fila['imagen'];
+                        $nombre = $fila['nombre'];
+                        $marca = $fila['marca'];
+
+                        echo "
                 <div class='producto'>
                 <div class='info'>
                     <img src='imagenes/$imagen'/>
@@ -46,26 +48,27 @@
                     <button onclick='eliminarProducto($id)'>Eliminar</button>
                 </div>
             </div>";
+                    }
+                } else {
+                    echo "No se encontraron resultados.";
+                }
+            } else {
+                die('Error al preparar la consulta: ' . $con->error);
             }
-        } else {
-            echo "No se encontraron resultados.";
-        }
-    } else {
-        die('Error al preparar la consulta: ' . $con->error);
-    }
 
-    $stmt->close();
-    $con->close();
+            $stmt->close();
+            $con->close();
+            ?>
+
+
+        </article>
+
+    </section>
+
+    <?php
+    include 'footer.php';
     ?>
 
-    
-    </article>
-   
-</section>
-  
-<?php
-    include 'footer.php';
-?>
-
 </body>
+
 </html>
