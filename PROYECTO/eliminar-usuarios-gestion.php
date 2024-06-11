0690c8094b082eb0c47281bef9e990bb9fd0837d
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-06-11 21:14:51 +0200

    margin bottom cuando solo hay 1 fila

--- PROYECTO/eliminar-usuarios-gestion.php
+++ PROYECTO/eliminar-usuarios-gestion.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,29 +8,29 @@
     <link rel="stylesheet" href="CSS/eliminar-usuarios-gestion.css">
     <script defer src='JS/eliminar-gestion.js'></script>
 </head>
+
 <body>
     <?php
     include 'header.php';
+    $contUsuarios = 0;
     ?>
 
     <section class="main">
-            
-    <?php
-    $con = new Conexion();
-    $con = $con->conectar();
-    // Preparar la consulta SQL
-    $select = "SELECT correoElectronico, tipo, id 
+
+        <?php
+        $con = new Conexion();
+        $con = $con->conectar();
+        $select = "SELECT correoElectronico, tipo, id 
             FROM usuario
             ORDER BY correoElectronico ASC";
-    $stmt = $con->prepare($select);
+        $stmt = $con->prepare($select);
 
-    // Verificar si la consulta se preparó correctamente
-    if ($stmt) {
-        $stmt->execute();
-        $result = $stmt->get_result();
+        if ($stmt) {
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-        if ($result->num_rows > 0) {
-            echo '<table>
+            if ($result->num_rows > 0) {
+                echo '<table>
             
                 <tr>
                     <th>Correo</th>
@@ -41,75 +42,75 @@
                         Cambiar
                     </th>
                 </tr>';
-            while ($fila = $result->fetch_assoc()) {
-                // Acceder a los valores de cada columna
-                $correo = $fila['correoElectronico'];
-                $tipo = $fila['tipo'];
-                $id = $fila['id'];
+                while ($fila = $result->fetch_assoc()) {
+                    $correo = $fila['correoElectronico'];
+                    $tipo = $fila['tipo'];
+                    $id = $fila['id'];
 
-                // Haz lo que necesites hacer con los datos de cada fila
-                // Por ejemplo, puedes imprimirlos en pantalla
-                echo "
-                <tr>
-                    <td>$correo</td>
-                    <td>".tipo_usuario($tipo)."</td>
-                    <td>
-                        <button onclick='eliminarUsuario($id)'>Eliminar</button>
-                    </td>
-                    <td>
-                        <button onclick='cambiar_tipo_usuario($id, $tipo)'>Cambiar a ".tipo_usuario_aCambiar($tipo)."</button>
-                    </td>
-                </tr>
-               
-            </div>";
+                    echo "
+                        <tr>
+                            <td>$correo</td>
+                            <td>" . tipo_usuario($tipo) . "</td>
+                            <td>
+                                <button onclick='eliminarUsuario($id)'>Eliminar</button>
+                            </td>
+                            <td>
+                                <button onclick='cambiar_tipo_usuario($id, $tipo)'>Cambiar a " . tipo_usuario_aCambiar($tipo) . "</button>
+                            </td>
+                        </tr>
+                    
+                    </div>";
+                    $contUsuarios++;
+                }
+                echo '</table>';
+            } else {
+                echo "No se encontraron resultados.";
             }
-            echo '</table>';
         } else {
-            echo "No se encontraron resultados.";
+            die('Error al preparar la consulta: ' . $con->error);
         }
-    } else {
-        // Si la consulta no se preparó correctamente, muestra un mensaje de error
-        die('Error al preparar la consulta: ' . $con->error);
-    }
 
-    $stmt->close();
-    $con->close();
+        $stmt->close();
+        $con->close();
 
 
-    //Para que en la tabla s emuestre con una palabra el tipo de usuario, ya que en la BBDD lo tenog con numeros
-    function tipo_usuario($tipo){
-        switch ($tipo) {
-            case 1: 
-                return 'Administrador';
-                break;
-            case 0:
-                return 'Usuario base';
-                break;
-            default:
-                return 'Tipo de usuario desconocido';
+        //Para que en la tabla s emuestre con una palabra el tipo de usuario, ya que en la BBDD lo tengo con numeros
+        function tipo_usuario($tipo)
+        {
+            switch ($tipo) {
+                case 1:
+                    return 'Administrador';
+                    break;
+                case 0:
+                    return 'Usuario base';
+                    break;
+                default:
+                    return 'Tipo de usuario desconocido';
+            }
         }
-    }
-    
-    //Esta funcion mostrará el cambio que se podrá hacer al darle al boton de cambiar a usuario base/administrador
-    function tipo_usuario_aCambiar($tipo){
-        switch ($tipo) {
-            case 1: 
-                return 'usuario base';
-                break;
-            case 0:
-                return 'administrador';
-                break;
-            default:
-                return 'Tipo de usuario desconocido';
+
+        //Esta funcion mostrará el cambio que se podrá hacer al darle al boton de cambiar a usuario base/administrador
+        function tipo_usuario_aCambiar($tipo)
+        {
+            switch ($tipo) {
+                case 1:
+                    return 'usuario base';
+                    break;
+                case 0:
+                    return 'administrador';
+                    break;
+                default:
+                    return 'Tipo de usuario desconocido';
+            }
         }
-    }
+        ?>
+
+    </section>
+
+    <?php
+    include 'footer.php';
     ?>
 
-</section>
-  
-<?php
-    include 'footer.php';
-?>
-
 </body>
+
 </html>
