b1759bd532779122a0ff5d46de90022f832add9f
michel-fdzzz <michel.fdzzz04@gmail.com>
2024-06-11 12:50:15 +0200

    Se muestra el codigo de pedido para saber que pedidos van juntos

--- PROYECTO/pedidos.php
+++ PROYECTO/pedidos.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pedidos</title>
     <link rel="stylesheet" href="CSS/pedidos.css">
 </head>
+
 <body>
     <?php include 'header.php'; ?>
-<section class="main">
+    <section class="main">
         <h1 class="tituloPedidos">Sus <br> pedidos</h1>
 
 
@@ -19,14 +21,14 @@
             $con = new Conexion();
             $con = $con->conectar();
 
-            $select = "select producto.imagen, pedido.nombreProducto, pedido.modelo, pedido.cantidad, pedido.precio, pedido.fecha
+            $select = "select producto.imagen, pedido.nombreProducto, pedido.modelo, pedido.cantidad, pedido.precio, pedido.fecha, pedido.id
             from pedido
             inner join producto on producto.id = pedido.idProducto
             where pedido.idCliente = ? 
             order by pedido.fecha desc";
             $stmt = $con->prepare($select);
 
-            if ($stmt){
+            if ($stmt) {
                 $stmt->bind_param("i", $_SESSION['idCliente']);
                 $stmt->execute();
                 $rest = $stmt->get_result();
@@ -39,7 +41,9 @@
                         <div class="pedido">
                             <img class="pedido-imagen" src="imagenes/' . $row['imagen'] . '" alt="Imagen del producto">
                             <hr>
-                            <p>'.$row['modelo'].' ' . $row['nombreProducto'] . '</p>
+                            <p><b>Código de pedido: ' . $row['id'] . '</b></p>
+                            <hr>
+                            <p>' . $row['modelo'] . ' ' . $row['nombreProducto'] . '</p>
                             <hr>
                             <p>Cantidad: ' . $row['cantidad'] . '</p>
                             <hr>
@@ -49,38 +53,39 @@
                         </div>';
                     }
                     echo '</article>';
-                    } else {
-                        echo '
+                } else {
+                    echo '
                         <div class="container-mensaje-pedidos">
                             <div class="mensajePredidos">
                                 <p>No has hecho ningún pedido</p>
                                 <a class="linkMensaje" href="index.php" target="_self"><div class="botonVerProductos"><p>Ver productos</p></div></a>
                             </div>
                         </div>';
-                    }
-                        ?>
+                }
+                ?>
 
-           
-              
-            </article>
+
+
+                </article>
                 <?php
             } else {
-              die('Error en la preparación de la consulta');
+                die('Error en la preparación de la consulta');
             }
             $con->close();
         } else {
-            echo  '
+            echo '
             <div class="mensaje">
                 <p>No has iniciado sesión, registrate o inicia sesión para poder ver tus productos en el carrito.</p>
                 <a class="linkMensaje" href="iniciarSesion.php" target="_self"><div class="boton"><p>Iniciar sesión/Registrarme</p></div></a>
             </div>';
         }
 
-        
-                ?>
+
+        ?>
 
 
     </section>
     <?php include 'footer.php'; ?>
 </body>
+
 </html>
